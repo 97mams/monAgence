39b95792188995d034439a66262c1fbbc8b43c90
97mams <97mams@gmail.com>
2024-02-25 09:59:39 +0300

    add pictures

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Property extends Model
@@ -35,6 +36,33 @@ class Property extends Model
     public function getSlug(): string
     {
         return Str::slug($this->title);
+    }
+
+    public function pictures(): HasMany
+    {
+        return $this->hasMany(Pictures::class);
+    }
+
+    public function attachFiles(array $files)
+    {
+        $prictures = [];
+        foreach ($files as $file) {
+            if ($file->getError()) {
+                continue;
+            }
+            $filename = $file->store('properties/' . $this->id, 'public');
+            $prictures[] = [
+                'filename' => $filename
+            ];
+        }
+        if (count($prictures) > 0) {
+            $this->pictures()->createMany($prictures);
+        }
+    }
+
+    public function getPicture(): ?Pictures
+    {
+        return $this->pictures[0] ?? null;
     }
 
     public function scopeAvailable(Builder $builder): Builder
